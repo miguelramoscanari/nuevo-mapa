13d54154e94c9fa47e5f65c223a4bc2cb6616c71
miguelramoscanari <mramos.pe@gmail.com>
2020-06-14 13:37:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1488,7 +1488,7 @@ heroe = sprites.create(img`
 . . . f f f f f f f f . . . . . 
 . . . . . f f . . f f . . . . . 
 `, SpriteKind.Player)
-heroe.say("RAFAEL")
+heroe.say("DORIS")
 controller.moveSprite(heroe)
 scene.cameraFollowSprite(heroe)
 inicioNivel()
